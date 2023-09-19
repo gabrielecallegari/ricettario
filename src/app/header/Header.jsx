0d8f2e8bb732c686a99342d5ba2aff67b7f0a5e1
Gabriele Callegari <g.callegari@itsrizzoli.it>
2023-09-19 19:19:51 +0200

    add: back home

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Header(){
 
+    const navigate = useNavigate()
+
+    const _handleClick = ()=>{
+        console.log("QUI");
+        navigate("/")
+    }
+
     return (
-        <div className="w-full shadow flex justify-center h-16">
-            <img alt="Ricettario.it" src={require("../../image/Ricettario.it.jpg")} className="object-contain"/>
+        <div className="w-full shadow flex justify-center h-16" >
+            <img alt="Ricettario.it" src={require("../../image/Ricettario.it.jpg")} className="object-contain" onClick={()=>_handleClick()} />
         </div>
     )
 }
