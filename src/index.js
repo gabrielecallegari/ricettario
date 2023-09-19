de2a84b3a29e91fa2a9c2221b43840462cdac60d
Gabriele Callegari <g.callegari@itsrizzoli.it>
2023-09-19 19:20:05 +0200

    add: react router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './app/home/Home';
+import NuovaRicetta from './app/nuovaricetta/NuovaRicetta';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "new",
+    element: <NuovaRicetta />
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
